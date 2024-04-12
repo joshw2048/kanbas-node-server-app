30c5d21ff50de0a100cf977098556e0625ede449
joshw2048 <joshw2048@gmail.com>
2024-04-12 15:04:29 -0400

    a6

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import express from 'express'
+import mongoose from "mongoose";
 import Hello from "./Hello.js"
 import Lab5 from './Lab5.js'
 import cors from "cors";
 import CourseRoutes from './Kanbas/courses/routes.js';
-import ModuleRoutes from './Kanbas/modules/routes.js';
+import UserRoutes from './Kanbas/Database/Users/routes.js';
+import ModuleRoutes from './Kanbas/Modules/routes.js';
+import "dotenv/config";
+import session from 'express-session';
 
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+app.use(session(sessionOptions));
+  
 app.listen(process.env.PORT || 4000);
-app.use(cors());
+const corsOptions = {
+  credentials: true,
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
